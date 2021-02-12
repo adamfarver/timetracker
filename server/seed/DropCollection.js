54a43529d3585c8dd1f2238701257b840193aabc
Adam Farver <adamfarver@gmail.com>
2021-02-12 15:01:17 -0500

    Add cron job to server Fixes #9

---
+++ server/seed/DropCollection.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const { localMongoServer } = require('../../config')
+
+//Models
+const Project = require('../../models/Project')
+const Role = require('../../models/Role')
+const Sprint = require('../../models/Sprint')
+const Task = require('../../models/Task')
+const Time = require('../../models/Time')
+const User = require('../../models/User')
+
+mongoose.connect(localMongoServer, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+
+const drop = async () => {
+	await Project.deleteMany({})
+	await Role.deleteMany({})
+	await Sprint.deleteMany({})
+	await Task.deleteMany({})
+	await Time.deleteMany({})
+	await User.deleteMany({})
+}
+
+exports.drop = drop
