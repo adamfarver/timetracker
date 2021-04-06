33e7db7017715e1e1b035fbb606e8c5bf8d4542e
Adam Farver <adamfarver@gmail.com>
2021-04-06 09:18:58 -0400

    Update files added

--- server/seed/updateData.js
+++ server/seed/updateData.js
@@ -1,18 +1,51 @@
 const fs = require('fs')
-const Moment = require('moment')
-
-let now = Moment()
+const moment = require('moment')
 
 let data = fs.readFileSync('./seedData.json', 'ascii')
 
 data = JSON.parse(data)
+
 // Start updating the file here.
 // Update the Sprint Start and End Data
-data.sprints[0].dateStart = now.format()
-data.sprints[0].dateEnd = now.add(7, 'days').format()
+;(function () {
+	let now = moment()
+	data.sprints[0].dateStart = now.subtract(4, 'days').format()
+	data.sprints[0].dateEnd = now.add(7, 'days').format()
+})()
+
 // Update Completed dates
 
+let taskUpdates = (function (data) {
+	let now = moment()
+	let completedTasks = data.tasks.filter((task) => {
+		return task.completed
+	})
+
+	let uncompletedTasks = data.tasks.filter((task) => {
+		return !task.completed
+	})
+	completedTasks = completedTasks.map((task) => {
+		task.updatedAt = now.subtract(1, 'day').format()
+		return task
+	})
+	let allTasks = completedTasks.concat(...uncompletedTasks)
+
+	return allTasks
+})(data)
+
+data.tasks = taskUpdates
 // Update In-Process Numbers
 
+// Write out data
+data = JSON.stringify(data)
+
+try {
+	fs.writeFileSync('./newSeedData.json', data)
+	console.log('File Written.')
+} catch (e) {
+	console.log(e)
+}
+
 // Console Output
-console.log(data.sprints)
+
+// console.log(taskUpdates)
