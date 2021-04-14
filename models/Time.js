2f5d50e62e2556df8d3ad48e20d33b269584ff98
Adam Farver <adamfarver@gmail.com>
2021-04-14 16:11:12 -0400

    Fix Time Model

--- models/Time.js
+++ models/Time.js
@@ -6,11 +6,13 @@ const mongoose = require('mongoose')
 const { ObjectId } = mongoose.Types
 module.exports = mongoose.model(
 	'time',
-	new mongoose.Schema({
-		taskId: { type: ObjectId, required: false },
-		timeUsed: { type: Number, required: false },
-		userId: { type: ObjectId, required: false },
-		sprint: { type: ObjectId, required: false },
-		createdAt: { type: Date },
-	})
+	new mongoose.Schema(
+		{
+			taskId: { type: ObjectId, required: false },
+			timeUsed: { type: Number, required: false },
+			userId: { type: ObjectId, required: false },
+			sprint: { type: ObjectId, required: false },
+		},
+		{ timestamps: { createdAt: 'created_at', modifiedAt: 'modified_at' } }
+	)
 )
