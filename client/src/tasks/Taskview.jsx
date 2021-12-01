7ec1049d2d98ca6a6c8bffd1528098043c5db98c
Adam Farver <adamfarver@gmail.com>
2021-12-01 09:42:28 -0500

    Bug fix - crashed taskview

--- client/src/tasks/Taskview.jsx
+++ client/src/tasks/Taskview.jsx
@@ -71,119 +71,121 @@ export function TaskView({ history, match }) {
 
 	return (
 		<>
-			<>
-				<Row>
-					<Col>
-						<Breadcrumbs />
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<div className="d-flex align-items-center">
-							<h1>{task.taskName}</h1>
-							{user.role.rolename === 'Manager' ||
-								('Admin' && (
-									<Col md>
-										<Link to={`/tasks/edit/${id}`}>
-											<FontAwesomeIcon icon="edit" className={'mr-1'} />
-											Edit Task
-										</Link>
-									</Col>
-								))}
-						</div>
-					</Col>
-				</Row>
-				<Row>
-					<Col md={3}>
-						<strong>Active:</strong>
-						<p>{task.active ? 'Yes' : 'No'}</p>
-					</Col>
+			{task !== {} && (
+				<>
+					<Row>
+						<Col>
+							<Breadcrumbs />
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<div className="d-flex align-items-center">
+								<h1>{task.taskName}</h1>
+								{user.role.rolename === 'Manager' ||
+									('Admin' && (
+										<Col md>
+											<Link to={`/tasks/edit/${id}`}>
+												<FontAwesomeIcon icon="edit" className={'mr-1'} />
+												Edit Task
+											</Link>
+										</Col>
+									))}
+							</div>
+						</Col>
+					</Row>
+					<Row>
+						<Col md={3}>
+							<strong>Active:</strong>
+							<p>{task.active ? 'Yes' : 'No'}</p>
+						</Col>
 
-					<Col md={3}>
-						<strong>Completed:</strong>
-						<p>{task.completed ? 'Yes' : 'No'}</p>
-					</Col>
-					<Col md={3}>
-						<strong>Projected Time:</strong>
-						<p>{task.projectedTime} hours</p>
-					</Col>
-					<Col md={3}>
-						{/* If completed, show who claimed */}
-						{task.completed && (
-							<>
-								<strong>Completed By:</strong>
-								<p>
-									{task.claimedBy.firstName} {task.claimedBy.lastName}
-								</p>
-							</>
-						)}
-						{/* If not completed and not claimed, show claim button */}
-						{!task.completed && !task.claimedBy._id && (
-							<Button
-								variant="success"
-								onClick={(event) => {
-									claimItem(event, task, user)
-								}}
-							>
-								Claim
-							</Button>
-						)}
-						{/* If not completed and claimed equals owner, show release button */}
-						{!task.completed &&
-							task.claimedBy._id &&
-							task.claimedBy._id === user._id && (
-								<Button
-									variant="danger"
-									onClick={(event) => {
-										claimItem(event, task, user)
-									}}
-								>
-									Release
-								</Button>
-							)}
-						{/* If not completed and claimed not equal owner, show who claimed*/}
-						{!task.completed &&
-							task.claimedBy._id &&
-							task.claimedBy._id !== user._id && (
+						<Col md={3}>
+							<strong>Completed:</strong>
+							<p>{task.completed ? 'Yes' : 'No'}</p>
+						</Col>
+						<Col md={3}>
+							<strong>Projected Time:</strong>
+							<p>{task.projectedTime} hours</p>
+						</Col>
+						<Col md={3}>
+							{/* If completed, show who claimed */}
+							{task.completed && (
 								<>
-									<strong>Claimed By:</strong>
+									<strong>Completed By:</strong>
 									<p>
 										{task.claimedBy.firstName} {task.claimedBy.lastName}
 									</p>
 								</>
 							)}
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<b>Additional Info:</b>
-						{task.additionalInfo ? <p>{task.additionalInfo}</p> : <p>None</p>}
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<h4>
-							<strong>Time Log</strong>
-						</h4>
-					</Col>
-				</Row>
-				<Row className="timelog py-3 mt-n1 ">
-					<Col>
-						<TaskViewTimesList times={times} setTimes={setTimes} />
-					</Col>
-
-					{user._id === task.claimedBy._id && (
-						<Col md={4}>
-							<AddEdit
-								match={match}
-								history={history}
-								times={times}
-								setTimes={setTimes}
-							/>
+							{/* If not completed and not claimed, show claim button */}
+							{!task.completed && !task.claimedBy._id && (
+								<Button
+									variant="success"
+									onClick={(event) => {
+										claimItem(event, task, user)
+									}}
+								>
+									Claim
+								</Button>
+							)}
+							{/* If not completed and claimed equals owner, show release button */}
+							{!task.completed &&
+								task.claimedBy._id &&
+								task.claimedBy._id === user._id && (
+									<Button
+										variant="danger"
+										onClick={(event) => {
+											claimItem(event, task, user)
+										}}
+									>
+										Release
+									</Button>
+								)}
+							{/* If not completed and claimed not equal owner, show who claimed*/}
+							{!task.completed &&
+								task.claimedBy._id &&
+								task.claimedBy._id !== user._id && (
+									<>
+										<strong>Claimed By:</strong>
+										<p>
+											{task.claimedBy.firstName} {task.claimedBy.lastName}
+										</p>
+									</>
+								)}
 						</Col>
-					)}
-				</Row>
-			</>
+					</Row>
+					<Row>
+						<Col>
+							<b>Additional Info:</b>
+							{task.additionalInfo ? <p>{task.additionalInfo}</p> : <p>None</p>}
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<h4>
+								<strong>Time Log</strong>
+							</h4>
+						</Col>
+					</Row>
+					<Row className="timelog py-3 mt-n1 ">
+						<Col>
+							<TaskViewTimesList times={times} setTimes={setTimes} />
+						</Col>
+
+						{user._id === task.claimedBy._id && (
+							<Col md={4}>
+								<AddEdit
+									match={match}
+									history={history}
+									times={times}
+									setTimes={setTimes}
+								/>
+							</Col>
+						)}
+					</Row>
+				</>
+			)}
 		</>
 	)
 }
