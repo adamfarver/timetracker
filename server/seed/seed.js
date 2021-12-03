27a958f189dcd4889b8469d7f4cfa776c65457a7
Adam Farver <adamfarver@gmail.com>
2021-12-03 16:23:28 -0500

    Fixing seed issues breaking server

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -11,21 +11,21 @@ const Time = require('../../models/Time')
 // create instance for client
 
 async function seed(data) {
-	await mongoose
-		.connect(mongoserver, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
-		.then(async () => {
-			await Project.insertMany(data.projects)
-			await User.insertMany(data.users)
-			await Task.insertMany(data.tasks)
-			await Role.insertMany(data.roles)
-			await Sprint.insertMany(data.sprints)
-			await Time.insertMany(data.times)
-			const now = new Date()
-			console.log('DB has been reset: \n' + now.toLocaleTimeString())
-		})
+	// await mongoose
+	// 	.connect(mongoserver, {
+	// 		useNewUrlParser: true,
+	// 		useUnifiedTopology: true,
+	// 	})
+	// 	.then(async () => {
+	await Project.insertMany(data.projects)
+	await User.insertMany(data.users)
+	await Task.insertMany(data.tasks)
+	await Role.insertMany(data.roles)
+	await Sprint.insertMany(data.sprints)
+	await Time.insertMany(data.times)
+	const now = new Date()
+	console.log('DB has been reset: \n' + now.toLocaleTimeString())
+	// })
 }
 
 exports.seed = seed
